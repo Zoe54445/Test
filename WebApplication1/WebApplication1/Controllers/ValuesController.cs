32b4c9ed6470fcb1097e42374a29b6d7d7969dd1
Zoe <zoechen54445@gmail.com>
2023-03-25 01:43:24 +0800

    新增

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DA;
+using WebApplication1.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public List<Customers> Get()
+        {
+            var result=new Customers_DA().GetAll();
+            return result;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public List<Customers> Get(string id)
+        {
+            var result = new Customers_DA().GetByCustomerID(id);
+            return result;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/DA/Customers_DA.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using WebApplication1.Models;
+namespace WebApplication1.DA
+{
+    public class Customers_DA
+    {
+        private readonly string _connectString =
+    @"Server=DESKTOP-0OHJKJ4\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=true;";
+
+        public List<Customers> GetAll()
+        {
+            string sqlQry = @"
+                           SELECT *
+  FROM [dbo].[Customers] 
+  WHERE customerid='ALFKI'
+                            ";
+
+            using (SqlConnection conn =  new SqlConnection(_connectString))
+            {
+                try
+                {
+                    return conn.Query<Customers>(sqlQry).ToList();
+                }
+                catch (Exception e)
+                {
+                    
+                }
+                return null;
+            }
+        }
+
+        public List<Customers> GetByCustomerID(string CustomerID)
+        {
+            string sqlQry = @"
+                           SELECT *
+  FROM [dbo].[Customers] 
+  WHERE CustomerID=@CustomerID
+                            ";
+
+            using (SqlConnection conn = new SqlConnection(_connectString))
+            {
+                try
+                {
+                    return conn.Query<Customers>(sqlQry,new { CustomerID }).ToList();
+                }
+                catch (Exception e)
+                {
+
+                }
+                return null;
+            }
+        }
+
+
+
+    }
+}
